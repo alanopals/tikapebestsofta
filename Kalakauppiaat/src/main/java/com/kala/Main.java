24d011d09fd1fd5281eb09ba924db1e554d19d57
hosseinbahmanpour <xbax@dhcp-asv-114.helsinki.fi>
2016-02-22 16:58:36 +0200

    pomi päiäitetty

--- Kalakauppiaat/src/main/java/com/kala/Main.java
+++ Kalakauppiaat/src/main/java/com/kala/Main.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.kala;
 
-/**
- *
- * @author xbax
- */
 public class Main {
+    
+    public static void main(String[] args) {
+        
+    }
     
 }
