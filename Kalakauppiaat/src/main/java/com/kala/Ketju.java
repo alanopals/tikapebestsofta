e49ac1199d67fdd742398d7d165048c534e0e29b
Yousif Abdullah <yousifabdullah@users.noreply.github.com>
2016-03-04 17:41:13 +0200

    Update Ketju.java

--- Kalakauppiaat/src/main/java/com/kala/Ketju.java
+++ Kalakauppiaat/src/main/java/com/kala/Ketju.java
@@ -1,41 +1,26 @@
 package com.kala;
 
-import java.sql.Timestamp;
-
 public class Ketju {
-
+    
     private final int id;
-    private final int palsta_id;
-    private final String nimimerkki;
+    private final int palstaId;
     private final String otsikko;
-    private final Timestamp aika;
 
-    public Ketju(int id, int palsta_id, String nimimerkki, String otsikko) {
+    public Ketju(int id, int palstaId, String otsikko) {
         this.id = id;
-        this.palsta_id = palsta_id;
-        this.nimimerkki = nimimerkki;
+        this.palstaId = palstaId;
         this.otsikko = otsikko;
-        this.aika = new Timestamp(new java.util.Date().getTime());
     }
 
     public int getId() {
         return id;
     }
 
-    public int getPalsta_id() {
-        return palsta_id;
-    }
-
-    public String getNimimerkki() {
-        return nimimerkki;
+    public int getPalstaId() {
+        return palstaId;
     }
 
     public String getOtsikko() {
         return otsikko;
     }
-
-    public Timestamp getAika() {
-        return aika;
-    }
-
 }
