a5df169a31e636f866f7927871f4ab5fb0aa8e47
Yousif Abdullah <yousifabdullah@users.noreply.github.com>
2016-03-04 17:41:28 +0200

    Update Palsta.java

--- Kalakauppiaat/src/main/java/com/kala/Palsta.java
+++ Kalakauppiaat/src/main/java/com/kala/Palsta.java
@@ -1,19 +1,20 @@
 package com.kala;
 
-import java.sql.Timestamp;
-
 public class Palsta {
 
     private final int id;
     private final String kuvaus;
-    private final String nimimerkki;
-    private final Timestamp aika;
 
-    public Palsta(int id, String kuvaus, String nimimerkki) {
+    public Palsta(int id, String kuvaus) {
         this.id = id;
         this.kuvaus = kuvaus;
-        this.nimimerkki = nimimerkki;
-        this.aika = new Timestamp(new java.util.Date().getTime());
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getKuvaus() {
+        return kuvaus;
+    }
 }
