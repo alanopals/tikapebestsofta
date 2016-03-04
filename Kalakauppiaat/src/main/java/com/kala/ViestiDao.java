8b4a0bfdb13d3501e408f6f4a41db99daa7a6469
Yousif Abdullah <yousifabdullah@users.noreply.github.com>
2016-03-04 17:39:16 +0200

    Update ViestiDao.java

--- Kalakauppiaat/src/main/java/com/kala/ViestiDao.java
+++ Kalakauppiaat/src/main/java/com/kala/ViestiDao.java
@@ -1,66 +1,100 @@
 package com.kala;
 
-import java.util.*;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViestiDao implements Dao<Viesti, Integer> {
-
+    
     private Database database;
-
-    public ViestiDao(Database database) {
+    
+    public ViestiDao(Database database) throws Exception {
         this.database = database;
+    }
+    
+    private List<Viesti> collect(ResultSet rs) throws Exception {
+        ArrayList<Viesti> viestit = new ArrayList<>();
+        
+        while (rs.next()) {
+            viestit.add(new Viesti(rs.getInt("id"), rs.getInt("ketju_id"), rs.getString("nimimerkki"), 
+            rs.getString("sisalto"), rs.getString("aika")));
+        }
+        
+        return viestit;
     }
 
     @Override
     public Viesti findOne(Integer key) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    @Override
+    public List<Viesti> findAll() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public List<Viesti> findAllIn(int ketju_id) throws SQLException {
         Connection connection = database.getConnection();
-
-        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Viesti WHERE id = ?");
-        stmt.setObject(1, key);
-
+        
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Viesti WHERE ketju_id = ?");
+        stmt.setInt(1, ketju_id);
+        
         ResultSet rs = stmt.executeQuery();
-        boolean hasOne = rs.next();
-
-        if (!hasOne) {
-            return null;
+        
+        List<Viesti> lista = null;
+        
+        collect: try {
+            lista = collect(rs);
+        } catch (Exception ex) {
+            break collect;
         }
-
-        int id = rs.getInt("id");
-        int ketju_id = rs.getInt("ketju_id");
-        String nimimerkki = rs.getString("nimimerkki");
-        String sisalto = rs.getString("sisalto");
-        Timestamp aika = rs.getTimestamp("aika");
-
-        Viesti v = new Viesti(id, ketju_id, nimimerkki, sisalto);
-
+        
         rs.close();
         stmt.close();
         connection.close();
-
-        return v;
+        
+        return lista;
     }
-
+    
     @Override
-    public List<Viesti> findAll() throws SQLException {
+    public void delete(Integer key) throws SQLException {
         Connection connection = database.getConnection();
+        
+        PreparedStatement stmt = connection.prepareStatement("DELETE FROM Viesti WHERE id = ?");
+        stmt.setObject(1, key);
+        
+        stmt.executeUpdate();
 
-        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Viesti");
-
+        stmt.close();
+        connection.close();
+    }
+    
+    @Override
+    public void add(Viesti v) throws SQLException {
+        Connection connection = database.getConnection();
+        
+        PreparedStatement stmt = connection.prepareStatement("INSERT INTO Viesti (ketju_id, nimimerkki, sisalto) VALUES(?, ?, ?)");
+        stmt.setInt(1, v.getKetju_id());
+        stmt.setString(2, v.getNimimerkki());
+        stmt.setString(3, v.getSisalto());
+        
+        stmt.executeUpdate();
+        
+        stmt.close();
+        connection.close();
+    }
+    
+    public int countAllIn(int ketju_id) throws SQLException{
+        Connection connection = database.getConnection();
+        
+        PreparedStatement stmt = connection.prepareStatement("SELECT COUNT(*) FROM Viesti WHERE ketju_id = ?");
+        stmt.setInt(1, ketju_id);
+        
         ResultSet rs = stmt.executeQuery();
-
-        List<Viesti> viestit = new ArrayList<>();
-
-        while (rs.next()) {
-
-            int id = rs.getInt("id");
-            int ketju_id = rs.getInt("ketju_id");
-            String nimimerkki = rs.getString("nimimerkki");
-            String sisalto = rs.getString("sisalto");
-            Timestamp aika = rs.getTimestamp("aika");
-
-            Viesti v = new Viesti(id, ketju_id, nimimerkki, sisalto);
-
-        }
+        int viestit = rs.getInt("COUNT(*)");
         
         rs.close();
         stmt.close();
@@ -68,10 +102,4 @@ public class ViestiDao implements Dao<Viesti, Integer> {
 
         return viestit;
     }
-
-    @Override
-    public void delete(Integer key) throws SQLException {
-        // ei toteutettu
-    }
-
 }
