9f5c9a3830e8bcc2fa4eb9ddc05448eaeef4d0d4
hosseinbahmanpour <xbax@dhcp-asv-114.helsinki.fi>
2016-03-11 16:41:24 +0200

    remppa päivitys

--- Kalakauppiaat/src/main/java/com/kala/Database.java
+++ Kalakauppiaat/src/main/java/com/kala/Database.java
@@ -3,13 +3,13 @@ package com.kala;
 import java.sql.*;
 
 public class Database {
-
+    
     private String databaseAddress;
-
+    
     public Database(String databaseAddress) throws ClassNotFoundException {
         this.databaseAddress = databaseAddress;
     }
-
+    
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(databaseAddress);
     }
