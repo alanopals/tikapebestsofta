cc2473f77c2060bfd53355c63974c8beb91b2fba
Yousif Abdullah <yousifabdullah@users.noreply.github.com>
2016-03-11 16:40:11 +0200

    Added files via upload

---
+++ TikapeFoorumi-master/src/main/java/com/kala/Viesti.java
@@ -0,0 +1,38 @@
+package com.kala;
+
+public class Viesti {
+    
+    private final int id;
+    private final int ketju_id;
+    private final String nimimerkki;
+    private final String sisalto;
+    private final String aika;
+    
+    public Viesti(int id, int ketjuId, String nimimerkki, String sisalto, String aika) {
+        this.id = id;
+        this.ketju_id = ketjuId;
+        this.nimimerkki = nimimerkki;
+        this.sisalto = sisalto;
+        this.aika = aika;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public int getKetju_id() {
+        return ketju_id;
+    }
+    
+    public String getNimimerkki() {
+        return nimimerkki;
+    }
+    
+    public String getSisalto() {
+        return sisalto;
+    }
+    
+    public String getAika() {
+        return aika;
+    }
+}
