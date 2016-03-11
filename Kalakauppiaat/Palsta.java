6ad55a73ac29b72504737be1d404fd677993d159
Yousif Abdullah <yousifabdullah@users.noreply.github.com>
2016-03-11 16:38:16 +0200

    Added files via upload

---
+++ Kalakauppiaat/Palsta.java
@@ -0,0 +1,20 @@
+package com.kala;
+
+public class Palsta {
+    
+    private final int id;
+    private final String kuvaus;
+    
+    public Palsta(int id, String kuvaus) {
+        this.id = id;
+        this.kuvaus = kuvaus;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public String getKuvaus() {
+        return kuvaus;
+    }
+}
